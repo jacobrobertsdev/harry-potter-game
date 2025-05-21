819ecf5c47d7cf68222e028c435a2cee522a8c17
Jacob Roberts <jacobroberts.dev@gmail.com>
2025-05-21 07:29:24 -0500

    Add house totals table to leaderboard

--- src/app/leaderboard/leaderboard.component.ts
+++ src/app/leaderboard/leaderboard.component.ts
@@ -1,10 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
-interface Player {
-  name: string;
-  score: string;
-  house: string;
-}
+import { Player } from '../player.service';
 
 @Component({
   selector: 'app-leaderboard',
@@ -12,15 +7,37 @@ interface Player {
   styleUrls: ['./leaderboard.component.css'],
 })
 export class LeaderboardComponent implements OnInit {
-  players: Player[] = [];
+  houseTotals: any = {};
+  leaderboard: Player[] = [];
 
   constructor() {}
 
   ngOnInit(): void {
-    this.players = JSON.parse(localStorage.getItem('leaderboard') || '[]');
+    this.leaderboard = JSON.parse(localStorage.getItem('leaderboard') || '[]');
+    this.houseTotals = {
+      gryffindor: this.calculateHouseTotals('gryffindor'),
+      slytherin: this.calculateHouseTotals('slytherin'),
+      hufflepuff: this.calculateHouseTotals('hufflepuff'),
+      ravenclaw: this.calculateHouseTotals('ravenclaw'),
+    };
+    console.log('House Totals:', this.houseTotals);
+  }
+
+  calculateHouseTotals(house: string): number {
+    let total;
+    if (this.leaderboard.length > 0) {
+      total = this.leaderboard
+        .filter(
+          (player: Player) =>
+            player.house && player.house.toLowerCase() === house.toLowerCase()
+        )
+        .reduce((total: number, player: Player) => total + player.score, 0);
+    }
+
+    return total || 0;
   }
 
   get sortedPlayers(): Player[] {
-    return this.players.sort((a, b) => parseInt(b.score) - parseInt(a.score));
+    return this.leaderboard.sort((a: Player, b: Player) => b.score - a.score);
   }
 }
